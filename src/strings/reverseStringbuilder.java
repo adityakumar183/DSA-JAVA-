3a104fe9a974cf7a3c584bafc9ce839fbdbb92ff
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-11 01:32:39 +0530

    aditya

---
+++ src/strings/reverseStringbuilder.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class reverseStringbuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("aditya kumar");
+        System.out.println(sb);
+
+        sb.reverse();
+        System.out.println(sb);
+
+    }
+}
