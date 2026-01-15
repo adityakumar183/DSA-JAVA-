3bd5ba0697f898a7e1ea7ec3d65b2b7969564bdf
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-15 22:55:50 +0530

    aditya

---
+++ src/LeetCode/StringCompression_443.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class StringCompression_443 {
+    public static void main(String[] args) {
+        String s = "aaaddittttyyyaaaa";
+        char[] arr = s.toCharArray();
+        String ans = "";
+        int i = 0 , j =0;
+        while(j<arr.length){
+            if(arr[j] == arr[i]) j++;
+            else{
+                ans += arr[i];
+                int len = j-i;
+                if(len > 1) ans += len;
+                i = j;
+            }
+        }
+        ans += arr[i];
+        int len = j-i;
+        if(len > 1) ans += len;
+        for(i = 0; i<ans.length(); i++){
+            arr[i] = ans.charAt(i);
+        }
+        System.out.println(ans);
+        System.out.println(ans.length());
+    }
+}
