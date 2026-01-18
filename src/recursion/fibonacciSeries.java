fd3c84ebf894efe7bbabfd3687f16f369a8f08e9
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-19 01:37:17 +0530

    aditya

---
+++ src/recursion/fibonacciSeries.java
@@ -0,0 +1,26 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class fibonacciSeries {
+    public static int fibonacci(int n){
+        // base case :-
+        // if(n==0 || n==1) return n;
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        // recursive work :-  find fibonacci(n-1) and fibonacci(n-2)
+        // self work :- sum of fibonacci(n-1) and fibonacci(n-2)
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("emter n :");
+        int n=sc.nextInt();
+
+        System.out.println("fibonacci number is : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i)+" ");
+        }
+    }
+}
