5e381cdcd0ba6b86143e52e8d408b79e610d76c2
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-19 23:22:43 +0530

    aditya

---
+++ src/recursion/sumOfDigit.java
@@ -0,0 +1,20 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class sumOfDigit {
+    public static int digitsum(int n){
+        // base case :-
+        if(n%10==n) return n;
+        // recursive work :- find (n/10)
+        // self work :- sum last digit
+        return digitsum(n/10)+(n%10);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number : ");
+        int n = sc.nextInt();
+
+        System.out.println("sum of digit is : " + digitsum(n));
+    }
+}
