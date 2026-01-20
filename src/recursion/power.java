bb624c4410e9a34c5dda5db211878d42cde597b5
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-20 23:21:52 +0530

    aditya

---
+++ src/recursion/power.java
@@ -0,0 +1,22 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class power {
+    public static int power(int p , int q){
+        // base case:-
+        if(q==0){
+            return 1;
+        }
+        return p * power(p ,q-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter base : ");
+        int p = sc .nextInt();
+        System.out.print("enter power : ");
+        int q = sc.nextInt();
+
+        System.out.println("value of " + p + "^" + q +"=" + power(p,q));
+    }
+}
