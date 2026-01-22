d993c712cbeec50031895d98597c2371ea37bd84
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-22 23:52:00 +0530

    aditya

---
+++ src/recursion/k_MultipleOfNumber.java
@@ -0,0 +1,23 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class k_MultipleOfNumber {
+    public static void multiple(int n , int k){
+        if(k==1){
+            System.out.println(n);
+            return;
+        }
+        multiple(n,k-1);
+        System.out.println(n*k);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
+        System.out.print("enter n : ");
+        int k = sc.nextInt();
+
+        multiple(n , k);
+    }
+}
