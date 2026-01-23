dbf3b03c6a88b821a5164d7a9597a2ac9d8be7ae
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-24 01:01:34 +0530

    aditya

--- src/recursion/GCD_M02.java
+++ src/recursion/GCD_M02.java
@@ -3,10 +3,19 @@ package recursion;
 import java.util.Scanner;
 
 public class GCD_M02 {
+    public static int GCD(int x,int y){
+        if(y==0){
+            return x;
+        }
+        return GCD(y,x%y);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
 
+        System.out.println("GCD is : " + GCD(x,y));
+        int LCM = (x*y)/GCD(x,y);
+        System.out.println("LCM is : " + LCM);
     }
 }
