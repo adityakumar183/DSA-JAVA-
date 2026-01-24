b12d761a1d1c799f8e6c547bec0c24a8bf43e98a
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-24 22:13:50 +0530

    aditya

--- src/recursion/GCD_M02.java
+++ src/recursion/GCD_M02.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 
 public class GCD_M02 {
     public static int GCD(int x,int y){
-        if(y==0){
-            return x;
-        }
+        if(y==0) return x;
         return GCD(y,x%y);
     }
     public static void main(String[] args) {
