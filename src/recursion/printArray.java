8556fe0734de3135637a978239923d95d0dbbb77
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-25 18:00:58 +0530

    aditya

---
+++ src/recursion/printArray.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class printArray {
+    public static void printarr(int[] arr , int idx){
+        // base case:-
+        if(idx == arr.length) return;
+
+        // self work:-
+        System.out.println(arr[idx]);
+
+        // recursive work:-
+        printarr(arr, idx+1);
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,6,7,9,11,24,56};
+
+        printarr(arr,0);
+    }
+}
