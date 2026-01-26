fc154afa0cf8a927b8f4143d1ba5589ec35e3ef0
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-26 20:30:30 +0530

    aditya

--- src/recursion/printArray.java
+++ src/recursion/printArray.java
@@ -6,7 +6,7 @@ public class printArray {
         if(idx == arr.length) return;
 
         // self work:-
-        System.out.println(arr[idx]);
+        System.out.print(arr[idx] +" ");
 
         // recursive work:-
         printarr(arr, idx+1);
@@ -15,6 +15,6 @@ public class printArray {
     public static void main(String[] args) {
         int[] arr = {2,3,5,6,7,9,11,24,56};
 
-        printarr(arr,0);
+        printarr(arr, 0);
     }
 }
