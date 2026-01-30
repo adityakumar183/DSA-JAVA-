f5e07a78ecce45c4fbccd8274b9e259f9a96859e
adityakumar183 <adityakumar26102004@gmail.com>
2026-01-31 00:08:54 +0530

    aditya

---
+++ src/recursion/removeAinString.java
@@ -0,0 +1,35 @@
+package recursion;
+import java.util.Scanner;
+public class removeAinString {
+    public static String remove(String s, int idx){
+        // base case:-
+        if(idx == s.length()){
+            return "";
+        }
+
+        // recursive work:-
+        String smallans = remove(s, idx+1);
+
+        // self work:-
+        if(s.charAt(idx) != 'a'){
+            return s.charAt(idx) + smallans;
+        }else{
+            return smallans;
+        }
+        // another method for self work:-
+//        char currChar = s.charAt(idx);
+//        if(currChar != 'a'){
+//            return currChar + smallans;
+//        }else {
+//            return smallans;
+//        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(remove(s,0));
+    }
+}
