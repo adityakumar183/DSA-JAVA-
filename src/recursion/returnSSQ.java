41e7ae542d7f03f7f872311dbcc2a692149eba3a
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-03 22:34:06 +0530

    aditya

---
+++ src/recursion/returnSSQ.java
@@ -0,0 +1,31 @@
+package recursion;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class returnSSQ {
+    public static ArrayList<String> getSSQ(String s) {
+        ArrayList<String> ans = new ArrayList<>();
+        // base case:-
+        if (s.length() == 0) {
+            ans.add("");
+            return ans;
+        }
+
+        char curr =  s.charAt(0);
+        ArrayList<String> smallans = getSSQ(s.substring(1));
+
+        for(String small : smallans){
+            ans.add(small);
+            ans.add(curr+small);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        ArrayList<String> smallans = getSSQ(s);
+        for(String small : smallans){
+            System.out.println(small);
+        }
+
+    }
+}
