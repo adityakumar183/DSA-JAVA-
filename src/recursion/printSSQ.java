345120ac120dcb9f02d80a36b7a03fd8452a3f63
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-05 20:22:42 +0530

    aditya

--- src/recursion/printSSQ.java
+++ src/recursion/printSSQ.java
@@ -2,13 +2,18 @@ package recursion;
 import java.util.Scanner;
 public class printSSQ {
     public static void printSSQ(String s,String CurrAns){
+        // base case:-
         if(s.length()==0){
             System.out.println(CurrAns);
             return;
         }
         char curr = s.charAt(0);
         String remString = s.substring(1);
-        printSSQ(remString,CurrAns+curr);
+
+        // curr char :- chooses to be a part of currAns
+        printSSQ(remString,CurrAns + curr);
+
+        // curr char :- does not choose to be a part
         printSSQ(remString,CurrAns);
     }
     public static void main(String[] args) {
