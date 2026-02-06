f14838c8ce6c71506838564dbe74e5aaae85e13c
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-06 13:22:46 +0530

    aditya

---
+++ src/recursion/frogJump.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class frogJump {
+    public static int best(int[] h,int n,int idx){
+        // base case :-
+        if(idx == n-1) return 0;
+        int op1 = Math.abs(h[idx] - h[idx+1]) + best(h,n,idx+1);
+        // base case:-
+        if(idx == n-2) return op1;
+        int op2 = Math.abs(h[idx] - h[idx+2]) + best(h,n,idx+2);
+
+        return Math.min(op1,op2);
+    }
+    public static void main(String[] args) {
+        int[] h = {10,30,40,20};
+        int n = h.length;
+
+        System.out.println(best(h,n,0));
+    }
+}
