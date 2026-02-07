8cdc9e91b66c452986c048393f4674d20dc008ae
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-07 22:58:16 +0530

    aditya

---
+++ src/recursion/one.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class one {
+        // Recursive function to calculate sum of digits
+        static int sumOfDigits(int n) {
+            if (n == 0) {
+                return 0;  // Base case
+            }
+            return (n % 10) + sumOfDigits(n / 10);  // Recursive call
+        }
+
+        public static void main(String[] args) {
+            int num = 1234;
+            System.out.println("Sum of digits of " + num + " is: " + sumOfDigits(num));
+        }
+}
+
