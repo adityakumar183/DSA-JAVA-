16acd92a1d5bee4070cd9d459cae0b4645c2e63f
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-09 00:01:12 +0530

    aditya

--- src/recursion/power_M02.java
+++ src/recursion/power_M02.java
@@ -3,11 +3,12 @@ package recursion;
 import java.util.Scanner;
 
 public class power_M02 {
-    public static int power(int p , int q){
+    public static double power(double p , int q){
         // base case:-
         if(q==0) return 1;
+        if(q<0) return 1/power(p,-q); // for negative power
 
-        int temp = power(p , q/2);
+        double temp = power(p , q/2);
         if(q%2==0){
             return temp * temp;                     // for even q
         }
@@ -16,7 +17,7 @@ public class power_M02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter base : ");
-        int p = sc .nextInt();
+        double p = sc .nextDouble();
         System.out.print("enter power : ");
         int q = sc.nextInt();
 
