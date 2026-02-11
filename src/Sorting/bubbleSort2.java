0a777538f150780423efbfa51c41f580fb42cf0e
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-11 18:38:53 +0530

    aditya

---
+++ src/Sorting/bubbleSort2.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class bubbleSort2 {
+    public static void bubblesort(int[] a){
+        int n = a.length;
+        // n-1 iterations
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                // last i element are already at correct sorted position,so no need to check them
+                if(a[j]<a[j+1]){            // for decreasing order......
+                    // swap - a[j] , a[j+1]
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int [] a = {7,5,6,4,3,2,1};
+        bubblesort(a);
+        for(int i : a){
+            System.out.print(i+" ");
+        }
+    }
+}
