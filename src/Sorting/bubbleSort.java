f66de0fb417d2d69ed8c5d4145351c5869276e9e
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-11 18:36:36 +0530

    aditya

--- src/Sorting/bubbleSort.java
+++ src/Sorting/bubbleSort.java
@@ -7,7 +7,7 @@ public class bubbleSort {
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
                 // last i element are already at correct sorted position,so no need to check them
-                if(a[j]>a[j+1]){
+                if(a[j]>a[j+1]){            // for increasing order......
                     // swap - a[j] , a[j+1]
                     int temp = a[j];
                     a[j] = a[j+1];
