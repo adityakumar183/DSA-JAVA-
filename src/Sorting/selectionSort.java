c4ca9b3da3e4d9d8d2303dd7a7961f5f0d1fed9b
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-12 22:15:58 +0530

    aditya

---
+++ src/Sorting/selectionSort.java
@@ -0,0 +1,47 @@
+package Sorting;
+
+public class selectionSort {
+    // Function to perform Selection Sort
+       public static void selectionSort(int arr[]) {
+            int n = arr.length;
+
+            for (int i = 0; i < n - 1; i++) {
+                // Assume the current index has the minimum
+                int minIndex = i;
+
+                // Find the index of the smallest element in the remaining array
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] < arr[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+
+                // Swap the found minimum element with the first element
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        // Function to print the array
+        static void printArray(int arr[]) {
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        // Main method
+        public static void main(String[] args) {
+            int arr[] = {64, 25, 12, 22, 11};
+
+            System.out.println("Original Array:");
+            printArray(arr);
+
+            selectionSort(arr);
+
+            System.out.println("Sorted Array:");
+            printArray(arr);
+        }
+    }
+
