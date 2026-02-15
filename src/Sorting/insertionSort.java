13e8629348bdbc897031fc110728b7e3a98ad375
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-16 03:38:16 +0530

    aditya

--- src/Sorting/insertionSort.java
+++ src/Sorting/insertionSort.java
@@ -1,18 +1,17 @@
 package Sorting;
-
 public class insertionSort {
     public static void insertionSort(int[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
+            int j = i;
 
             // Move elements greater than key one position ahead
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
             }
-            arr[j + 1] = key;
         }
     }
 
