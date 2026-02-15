75e6a0521ce075e21fd895cd4b835551cf9a9f88
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-15 17:16:07 +0530

    aditya

--- src/Sorting/bubbleSort2.java
+++ src/Sorting/bubbleSort2.java
@@ -5,6 +5,7 @@ public class bubbleSort2 {
         int n = a.length;
         // n-1 iterations
         for(int i=0;i<n-1;i++){
+            boolean flag = false;
             for(int j=0;j<n-i-1;j++){
                 // last i element are already at correct sorted position,so no need to check them
                 if(a[j]<a[j+1]){            // for decreasing order......
@@ -12,7 +13,11 @@ public class bubbleSort2 {
                     int temp = a[j];
                     a[j] = a[j+1];
                     a[j+1] = temp;
+                    flag = true;
                 }
+            }
+            if(!flag){
+                return;
             }
         }
     }
--- src/Sorting/selectionSort.java
+++ src/Sorting/selectionSort.java
@@ -10,15 +10,15 @@ public class selectionSort {
 
                 // Find the index of the smallest element in the remaining array
                 for (int j = i + 1; j < n; j++) {
-                    if (arr[j] < arr[minIndex]) {
+                    if (arr[j] < arr[minIndex]) {      // decreasing order me karne ke liye (>) use this
                         minIndex = j;
                     }
                 }
 
                 // Swap the found minimum element with the first element
-                int temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
             }
         }
 
