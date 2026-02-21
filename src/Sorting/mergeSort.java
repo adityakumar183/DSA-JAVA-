f3e2ddb1a572d54c70b36a5eb2aef70a1622cddf
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-21 16:29:29 +0530

    aditya

--- src/Sorting/mergeSort.java
+++ src/Sorting/mergeSort.java
@@ -2,48 +2,57 @@ package Sorting;
 
 public class mergeSort {
         // Merge two halves
-       public static void merge(int[] arr, int left, int mid, int right) {
-            int n1 = mid - left + 1;
-            int n2 = right - mid;
+       public static void merge(int[] arr, int l, int mid, int r) {
+            int n1 = mid - l + 1;
+            int n2 = r - mid;
 
-            int[] L = new int[n1];
-            int[] R = new int[n2];
+            int[] left = new int[n1];
+            int[] right = new int[n2];
 
             for (int i = 0; i < n1; i++)
-                L[i] = arr[left + i];
+                left[i] = arr[l + i];
             for (int j = 0; j < n2; j++)
-                R[j] = arr[mid + 1 + j];
+                right[j] = arr[mid + 1 + j];
 
-            int i = 0, j = 0, k = left;
+            int i = 0, j = 0, k = l;
             while (i < n1 && j < n2) {
-                if (L[i] <= R[j]) {
-                    arr[k++] = L[i++];
+                if (left[i] <= right[j]) {
+                    arr[k++] = left[i++];
                 } else {
-                    arr[k++] = R[j++];
+                    arr[k++] = right[j++];
                 }
             }
 
-            while (i < n1) arr[k++] = L[i++];
-            while (j < n2) arr[k++] = R[j++];
+            while (i < n1) arr[k++] = left[i++];
+            while (j < n2) arr[k++] = right[j++];
         }
 
         // Recursive sort
-       public static void mergeSort(int[] arr, int left, int right) {
-            if (left < right) {
-                int mid = (left + right) / 2;
-                mergeSort(arr, left, mid);
-                mergeSort(arr, mid + 1, right);
-                merge(arr, left, mid, right);
+       public static void mergeSort(int[] arr, int l, int r) {
+            if (l >= r) return;
+            int mid = (l + r) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
+
+        // display function
+        public static void display(int[] arr){
+           for(int val : arr){
+               System.out.print(val+" ");
             }
         }
 
         public static void main(String[] args) {
             int[] arr = {5, 2, 9, 1, 5, 6};
-            mergeSort(arr, 0, arr.length - 1);
+            int n = arr.length;
+            System.out.println("Original Array:");
+            display(arr);
+            System.out.println();
+
+            mergeSort(arr, 0, n-1);
 
             System.out.println("Sorted array:");
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-        }
-    }
+            display(arr);
+       }
+}
