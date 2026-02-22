103f1ee50c0ae72ba29eb30e17035e0568b80405
adityakumar183 <adityakumar26102004@gmail.com>
2026-02-22 18:45:09 +0530

    aditya

--- src/Sorting/quickSort.java
+++ src/Sorting/quickSort.java
@@ -1,21 +1,41 @@
 package Sorting;
 
 public class quickSort {
-    public static void quicksort(int[] arr, int st,int end){
-
-        for(int i=st;i<=end;i++) {
-            int pivot = arr[st];
-            for (int j = st + 1; j < end; j++) {
-                if (arr[st] > arr[j]) {
-                    swap(arr, st, j);
-                }
+    public static int partition(int[] arr,int low,int high){
+        int pivot = arr[low], pidx = low;
+        int smallcount = 0;
+        for (int i = low+1; i <= high; i++) {
+            if (arr[i] <= pivot) {
+                smallcount ++;
             }
         }
+        int correctidx = pidx + smallcount;
+        swap(arr,pidx,correctidx);
+        // partition
+        int i = low;
+        int j = high;
+        while (i<correctidx && j>pidx) {
+            if (arr[i] <= pivot) i++;
+            else if (arr[j] > pivot) j--;
+            else if(arr[i]>pivot && arr[j]<=pivot){
+                swap(arr,i,j);
+            }
+        }
+        return correctidx;
     }
+
+    public static void quicksort(int[] arr, int lo,int hi){
+        if(lo>=hi) return;
+        int idx = partition(arr,lo,hi);
+        quicksort(arr,lo,idx-1);
+        quicksort(arr,idx+1,hi);
+    }
+
     public static void display(int [] arr){
         for(int val : arr){
             System.out.print(val+" ");
         }
+        System.out.println();
     }
 
     public static void swap(int[] arr,int a,int b){
@@ -25,11 +45,10 @@ public class quickSort {
     }
 
     public static void main(String[] args) {
-        int [] arr = {6,3,8,2,3,5,7};
+        int [] arr = {6,3,8,2,1,4,5,7};
         int n = arr.length;
         System.out.println("Before sorting: ");
         display(arr);
-        System.out.println();
 
         quicksort(arr,0,n-1);
         System.out.println("After sorting: ");
