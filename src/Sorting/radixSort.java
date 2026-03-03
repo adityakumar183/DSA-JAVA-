44fa7f743fba8ad45d6ac2792c0ecc5e3627309c
adityakumar183 <adityakumar26102004@gmail.com>
2026-03-03 17:03:42 +0530

    aditya

--- src/Sorting/radixSort.java
+++ src/Sorting/radixSort.java
@@ -1,57 +1,66 @@
 package Sorting;
 import java.util.*;
 public class radixSort {
-    // Function to get maximum value in array
-    static int getMax(int arr[]) {
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++)
-            if (arr[i] > max)
+    public static int findmax(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
                 max = arr[i];
-          return max;
-        }
-
-        // Counting sort for a specific digit place
-        static void countingSort(int arr[], int exp) {
-            int n = arr.length;
-            int output[] = new int[n]; // sorted array
-            int count[] = new int[10]; // digit count (0-9)
-
-            // Count occurrences of digits
-            for (int i = 0; i < n; i++)
-                count[(arr[i] / exp) % 10]++;
-
-            // Convert count[] to cumulative count
-            for (int i = 1; i < 10; i++)
-                count[i] += count[i - 1];
-
-            // Build output array (stable sorting)
-            for (int i = n - 1; i >= 0; i--) {
-                int digit = (arr[i] / exp) % 10;
-                output[count[digit] - 1] = arr[i];
-                count[digit]--;
             }
+        }
+        return max;
+    }
 
-            // Copy sorted values back to arr[]
-            for (int i = 0; i < n; i++)
-                arr[i] = output[i];
+    public static void countsort(int[] arr, int place){
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+        for(int i = 0; i < n; i++){    // make frequency array
+            count[(arr[i] / place) % 10]++;
         }
 
-        // Main Radix Sort function
-        static void radixSort(int arr[]) {
-            int max = getMax(arr);
-
-            // Apply counting sort for each digit place
-            for (int exp = 1; max / exp > 0; exp *= 10)
-                countingSort(arr, exp);
+        // prefix sum array of count array
+        for(int i = 1; i < count.length; i++){
+            count[i] += count[i - 1];
         }
 
-        // Driver code
-        public static void main(String[] args) {
-            int[] arr ={170, 45, 75, 90, 802, 24, 2, 66};
+        for(int i = n - 1; i >= 0; i--){
+            int idx = count[(arr[i] / place) % 10] - 1;
+            output[idx] = arr[i];
+            count[(arr[i] / place) % 10]--;
+        }
 
-            System.out.println("Original Array: " + Arrays.toString(arr));
-            radixSort(arr);
-            System.out.println("Sorted Array:   " + Arrays.toString(arr));
-     }
+        // copy all elements of output to array
+        for(int i = 0; i < n; i++){
+            arr[i] = output[i];
+        }
+    }
+
+    public static void radixsort(int[] arr){
+        int max = findmax(arr);
+
+        for(int place = 1; max/place > 0; place *= 10){
+            countsort(arr,place);
+        }
+    }
+
+    public static void display(int[] arr){
+        for(int val : arr){
+            System.out.print(val+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {22,55,88,43,667,134,8345,9652,10758};
+        int n = arr.length;
+        System.out.println("unsorted array : ");
+        display(arr);
+
+        radixsort(arr);
+
+        System.out.println("Sorted array: ");
+        display(arr);
+    }
 }
 
