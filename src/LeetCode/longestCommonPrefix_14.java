a7bc9a7f5a54405c31ae994d5559d2960ca7b3ab
adityakumar183 <adityakumar26102004@gmail.com>
2026-03-23 00:33:28 +0530

    aditya

---
+++ src/LeetCode/longestCommonPrefix_14.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class longestCommonPrefix_14 {
+    public static String longestCommonPrefix(String[] str) {
+        if (str == null || str.length == 0) {
+            return "";
+        }
+
+        String prefix = str[0];
+        for (int i = 1; i < str.length; i++) {
+            while (str[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] str1 = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(str1));
+    }
+}
