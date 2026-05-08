81f3ed017a004fb2ac4f2ac8f8e5a2fa2d44e07c
adityakumar183 <adityakumar26102004@gmail.com>
2026-05-08 21:22:16 +0530

    aditya

---
+++ src/LeetCode/middleOfTheLinkedList_876.java
@@ -0,0 +1,42 @@
+package LeetCode;
+
+public class middleOfTheLinkedList_876 {
+    // two pass solution:-      T.C = O(n)  and  O(1) extra space is used
+    public static ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int len = 0;
+        while(temp != null){
+            temp = temp.next;
+            len++;
+        }
+        int mid = len/2 + 1;
+        temp = head;
+        for(int i = 1;i<=mid-1;i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        ListNode mid = middleNode(head);
+        System.out.print("List from Middle: ");
+        printList(mid);
+    }
+}
