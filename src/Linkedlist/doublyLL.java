61ff7b992f99413d6534c4d50dde3cfe853844aa
adityakumar183 <adityakumar26102004@gmail.com>
2026-05-26 17:06:15 +0530

    aditya

--- src/Linkedlist/doublyLL.java
+++ src/Linkedlist/doublyLL.java
@@ -43,6 +43,39 @@ public class doublyLL {
         System.out.println();
     }
 
+    public static Node insertAtHead(Node head, int x){
+        Node t = new Node(x);
+        t.next = head;
+        head.prev = t;
+        head = t;
+        return head;
+    }
+
+    public static void insertAtTail(Node head, int x){
+        Node temp = head;
+        // temp ko tail tail tak le kar jaate hai
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        Node t = new Node(x);
+        temp.next = t;
+        t.prev = temp;
+    }
+
+    public static void insertAtIdx(Node head, int idx, int x){
+        Node s = head;
+        for(int i=1;i<=idx-1;i++){
+            s = s.next;
+        }
+        // s is at idx-1 position
+        Node r = s.next;  // idx
+        Node t = new Node(x);
+        s.next = t;
+        t.prev = s;
+        t.next = r;
+        r.prev = t;
+    }
+
     public static void main(String[] args) {
         // 4 10 2 99 13
         Node a = new Node(4);
@@ -61,5 +94,17 @@ public class doublyLL {
         display(a);
         displayReverse(e);
         displayRandom(c);
+
+        // value insert at head
+        Node newHead = insertAtHead(a,35);
+        display(newHead);
+
+        // value insert at tail
+        insertAtTail(a,90);
+        display(a);
+
+        // value insert at idx
+        insertAtIdx(a,3,56);
+        display(a);
     }
 }
