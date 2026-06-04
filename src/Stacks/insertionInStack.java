1253736b7745a23efd2c3ca76c06c97a49016b7c
adityakumar183 <adityakumar26102004@gmail.com>
2026-06-04 23:37:00 +0530

    Insertion In Stack

---
+++ src/Stacks/insertionInStack.java
@@ -0,0 +1,29 @@
+package Stacks;
+import java.util.Stack;
+
+public class insertionInStack {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println("Stack is : " + st);
+
+        int idx = 2;
+        int x = 7;
+        Stack<Integer> temp = new Stack<>();
+        while(st.size()>idx){
+            temp.push(st.pop());
+        }
+        st.push(x);
+        while(temp.size()>0){
+            st.push(temp.pop());
+        }
+        System.out.println("New Stack is : " + st);
+    }
+}
+
+
+// agar index value stack ke size se bda hai to value last me insert ho jayega
