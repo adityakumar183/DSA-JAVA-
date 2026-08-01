701ce46e1f123d003e8843089d439da457092dce
adityakumar183 <adityakumar26102004@gmail.com>
2026-08-01 17:54:58 +0530

    Size of Binary Tree

---
+++ src/Trees/sizeOfBinaryTree.java
@@ -0,0 +1,29 @@
+package Trees;
+
+public class sizeOfBinaryTree {
+    public static int size(Node root) {
+        if (root == null) return 0;
+        return 1 + size(root.left) + size(root.right);
+    }
+    public static void main(String[] args) {
+        Node a = new Node(1);   //  a is the root
+        Node b = new Node(4);
+        Node c = new Node(3);
+        Node d = new Node(2);
+        Node e = new Node(6);
+        Node f = new Node(5);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+
+        Node g = new Node(10);
+        Node h = new Node(20);
+        c.left = g;
+        e.right = h;
+
+        System.out.println("Size of Binary tree is = " + size(a));
+    }
+}
